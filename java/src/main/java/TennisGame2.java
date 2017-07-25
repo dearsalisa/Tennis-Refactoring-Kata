71247e9d2673f18d9270deaf297d2524181e7abe
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-25 16:39:37 +0700

    delete score(var) and refactor advantage&win case

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,6 +1,5 @@
+public class TennisGame2 implements TennisGame {
 
-public class TennisGame2 implements TennisGame
-{
     public int P1point = 0;
     public int P2point = 0;
     private String player1Name;
@@ -11,61 +10,50 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public void P1Score(){ P1point++; }
-    public void P2Score(){ P2point++; }
+    public void P1Score() {
+        P1point++;
+    }
 
-    public String getScore(){
+    public void P2Score() {
+        P2point++;
+    }
+
+    public String getScore() {
 
         String[] scoreText = {"Love", "Fifteen", "Thirty", "Forty", ""};
-        String score = "";
 
-//        if (isEqualScore()) {
-//        } else if (isNormalScore()) {
-//        } else if (isDeuce()) {
-//            return "Deuce";
-//        } else if (isAdvantage()) {
-//        } else if (isWin()) {
-//        } else return "Invalid Input";
-
-
-        if (P1point == P2point && P1point < 4)
-        {
-            score = scoreText[P1point]+"-All";
+        if (P1point == P2point && P1point <= 2) {
+            return scoreText[P1point] + "-All";
         }
-        if (P1point==P2point && P1point>=3) {
-            score = "Deuce";
+        if (P1point == P2point && P1point >= 3) {
+            return "Deuce";
         }
-        if ( P1point != P2point && P2point < 4 && P1point < 4)
-        {
-            score = scoreText[P1point] + "-" + scoreText[P2point];
+        if (P1point != P2point && P2point < 4 && P1point < 4) {
+            return scoreText[P1point] + "-" + scoreText[P2point];
         }
-
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1";
+        if (Math.abs(P1point - P2point) == 1) {
+            if (P1point > P2point) {
+                return String.format("Advantage %s", player1Name);
+            }
+            if (P2point > P1point) {
+                return String.format("Advantage %s", player2Name);
+            }
         }
-
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
+        if (Math.abs(P1point - P2point) >= 2) {
+            if (P1point > P2point) {
+                return String.format("Win for %s", player1Name);
+            }
+            if (P2point > P1point) {
+                return String.format("Win for %s", player2Name);
+            }
         }
-
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
-        }
-        return score;
+        return "";
     }
+
     public void wonPoint(String player) {
         if (player == "player1")
             P1Score();
         else
             P2Score();
     }
-
-
 }
